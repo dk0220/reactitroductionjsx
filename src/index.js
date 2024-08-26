b6ce92061262b90a3391563864844b0b338b939e
dk0220 <138858303+dk0220@users.noreply.github.com>
2024-08-26 19:52:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is my paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
